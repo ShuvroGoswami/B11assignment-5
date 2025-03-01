419bbb36c9b862d23f12148294486c8fc5f5362a
ShuvroGoswami <shuvrogo@gmail.com>
2025-03-01 15:50:42 +0600

    3rd commit

---
+++ all-js-file/add-minous.js
@@ -0,0 +1,22 @@
+
+
+let number = [23];
+
+let numb = [5]
+
+ function plusMinusNumber(){
+  number++;
+  numb--;
+
+  document.getElementById('add-number').textContent = number
+
+  document.getElementById('mionus-number').textContent = numb;
+ }
+
+
+//  let num = [5];
+
+// function plusMinusNumber(){
+//  num--;
+//  document.getElementById('mionus-number').textContent = num
+// }
